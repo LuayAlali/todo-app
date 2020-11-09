b58f21bb4146b6ed924cde35ab826ad052a02f49
Luay Alali <luayalali@gmail.com>
2020-11-09 14:33:22 -0800

    created file structure.Components Form, TodoList added to App. font added to public index.html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+// Importing components
+import Form from './components/Forms';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello React</h1>
+    <header>
+     <h1>Luay's Todo List</h1>
+     </header>
+     <Form />
+     <TodoList />
     </div>
   );
 }
